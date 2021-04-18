2d0e847c1b5eccc96992b4d1294fa76022dc6309
Andrew Kolos <andrewrkolos@gmail.com>
2021-04-18 09:08:04 -0400

    add MaterialUI

---
+++ components/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <p>This is the app component.</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ components/layout.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  appBar: {
+    display: 'inline',
+    margin: 'auto',
+  },
+  footer: {
+    marginTop: '3rem',
+    paddingTop: '0.5rem',
+    borderTop: '1px solid #dddddd',
+    textAlign: 'center',
+  },
+});
+
+/**
+ * The common layout for all pages.
+ */
+const Layout: React.FC = (props) => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.appBar} variant="h6">
+            Pokemon Card Maker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Head>
+        <title>Pokemon Card Maker</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <main>{props.children}</main>
+
+      <footer className={classes.footer}>Here is the footer.</footer>
+    </React.Fragment>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
