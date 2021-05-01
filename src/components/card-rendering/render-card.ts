f1a0b3787dbfe24f9053a1ec3dbe6130ac7f1d1e
Andrew Kolos <andrewrkolos@gmail.com>
2021-04-30 20:31:34 -0400

    fix word wrap

--- src/components/card-rendering/render-card.ts
+++ src/components/card-rendering/render-card.ts
@@ -3,35 +3,45 @@ import { CardCanvas } from './card-canvas';
 
 function isSunMoonTrainerCard(card: Card): card is SunMoonTrainerCard {
   const asPokemonCard = card as Partial<SunMoonTrainerCard>;
-  return (asPokemonCard.trainerType != null && asPokemonCard.prismStar != null);
+  return asPokemonCard.trainerType != null && asPokemonCard.prismStar != null;
 }
 
 export async function renderCard(cardData?: Card): Promise<string> {
   const canvas = new CardCanvas();
   console.log(cardData);
 
-  if (cardData === undefined){
-
-  } else if (isSunMoonTrainerCard(cardData)){
+  if (cardData === undefined) {
+  } else if (isSunMoonTrainerCard(cardData)) {
     // find template
     const trainerType = cardData.trainerType;
     const prismStar = cardData.prismStar;
     const fullArt = cardData.fullArt;
 
-    const template = prismStar ? 'SunMoonTrainers/Prism.png' : (trainerType == 'Stadium' ? 'SunMoonTrainers/Stadium.png' : (fullArt ? `SunMoonTrainers/${trainerType}FA.png` : `SunMoonTrainers/${trainerType}.png`));
-    if (cardData.image){canvas.drawImage(0,0,cardData.image);}
-    canvas.drawImage(0,0,template);
+    const template = prismStar
+      ? 'SunMoonTrainers/Prism.png'
+      : trainerType == 'Stadium'
+      ? 'SunMoonTrainers/Stadium.png'
+      : fullArt
+      ? `SunMoonTrainers/${trainerType}FA.png`
+      : `SunMoonTrainers/${trainerType}.png`;
+    if (cardData.image) {
+      canvas.drawImage(0, 0, cardData.image);
+    }
+    canvas.drawImage(0, 0, template);
     canvas.drawText({
       color: 'black',
+      wrap: {
+        baselineY: 500,
+      },
       text: cardData.effect,
       fontName: 'FuturaStd',
       fontSize: 48,
       x: 17,
       y: 250,
-      maxWidth: 900,
+      maxWidth: 600,
       stroke: {
         color: 'white',
-        width: 1
+        width: 1,
       },
     });
     canvas.drawText({
@@ -44,7 +54,7 @@ export async function renderCard(cardData?: Card): Promise<string> {
       maxWidth: 300,
       stroke: {
         color: 'white',
-        width: 1
+        width: 1,
       },
     });
   }
